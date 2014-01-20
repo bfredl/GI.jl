827bad887d50b43393f450f5a45d9e0be050c860
Björn Linse <bjorn.linse@gmail.com>
2014-01-20 21:59:14 +0100

    more tests and document the state of things

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 Julia bindings using libgobject-introspection.
 
 This is still in a very early stage.
+
+What works:
+* GObject types, with constructors and methods (including property accessors)
+* passing of numeric types, string constants, gobjects
+* runtime type-determination of returned GObject pointers
+* Full type compatibility with Gtk.jl 
+* properties and signal handling (from Gtk.jl)
+* constants and enums/flags 
+* multiple return values using c-pointers
+* Basic error handling
+
+What needs to be done:
+* passing of arrays
+* memory management still quite rough
+* proper support for structs and abstract interfaces
+* consistent naming of things
+* callback parameters
+* much more...
--- test/test_embedclutter.jl
+++ test/test_embedclutter.jl
@@ -2,8 +2,7 @@ using Gtk
 using GI
 
 @gimport GtkClutter init, Embed(get_stage) 
-@gimport Clutter Actor, Stage(set_color), Text(new_with_text), Container(add_actor)
-@gimport Gtk Widget(set_size_request)
+@gimport Clutter Actor(get_flags,set_reactive), Stage(set_color), Text(new_with_text), Container(add_actor)
 
 GtkClutter.init(0,C_NULL)
 
@@ -17,9 +16,21 @@ set_color(stage, color)
 txt = new_with_text("Sans 20", "Clutter text")
 add_actor(stage,txt)
 
+#enums
+@assert get_flags(txt) & Clutter.ActorFlags.REACTIVE == 0
+set_reactive(txt,true)
+@assert get_flags(txt) & Clutter.ActorFlags.REACTIVE != 0
+
+
+# GI inherits GObject support from Gtk.jl, for instance properties, signals
+print(txt)
+signal_connect(txt,"button-press-event") do actor,event
+    print("clicked\n")
+end
+
 vbox = GtkBox(:v)
 push!(vbox,embed)
 push!(vbox,GtkLabel("Gtk text"))
 window = GtkWindow(vbox, "GtkClutter Test", 300, 300)
-
+nothing
 
